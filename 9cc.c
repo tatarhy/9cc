86aa1d0ce1587c466cca35d4fd6a603f8b25f02d
tatarhy <tat.j.kikyo@gmail.com>
2019-01-22 22:41:46 +0900

    add vector

--- 9cc.c
+++ 9cc.c
@@ -14,6 +14,53 @@ typedef struct {
     char *input;
 } Token;
 
+typedef struct {
+    void **data;
+    int capacity;
+    int len;
+} Vector;
+
+Vector *new_vector() {
+    Vector *vec = malloc(sizeof(Vector));
+    vec->data = malloc(sizeof(void *) * 16);
+    vec->capacity = 16;
+    vec->len = 0;
+    return vec;
+}
+
+void vec_push(Vector *vec, void *elem) {
+    if (vec->capacity == vec->len) {
+        vec->capacity *= 2;
+        vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
+    }
+    vec->data[vec->len++] = elem;
+}
+
+void expect(int line, int expected, int actual) {
+    if (expected == actual) {
+        return;
+    }
+    fprintf(stderr, "%d: %d expected, but got %d\n", line, expected, actual);
+    exit(1);
+}
+
+void runtest() {
+    Vector *vec = new_vector();
+    expect(__LINE__, 0, vec->len);
+
+    for (int i = 0; i < 100; i++) {
+        vec_push(vec, (void *)i);
+    }
+
+    expect(__LINE__, 100, vec->len);
+    expect(__LINE__, 0, (int)vec->data[0]);
+    expect(__LINE__, 50, (int)vec->data[50]);
+    expect(__LINE__, 99, (int)vec->data[99]);
+
+    printf("OK\n");
+}
+
+
 Token tokens[100];
 int pos = 0;
 
@@ -177,6 +224,11 @@ int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "Invalid arguments\n");
         return 1;
+    }
+
+    if (strcmp(argv[1], "-test") == 0) {
+        runtest();
+        return 0;
     }
 
     tokenize(argv[1]);
